e13350dfc2a5d0d75997b9680f4ae23c45547140
Yuriy Chepel <yuriy.chepel@gmail.com>
2024-04-25 19:31:04 +0200

    init empty project

---
+++ src/main/java/de/ait_tr/g_38_jp_shop/Group38JavaProShopApplication.java
@@ -0,0 +1,13 @@
+package de.ait_tr.g_38_jp_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group38JavaProShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group38JavaProShopApplication.class, args);
+	}
+
+}
